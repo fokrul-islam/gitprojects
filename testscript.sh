6d44803fd55d3dcb9ce6b2c96d80abb96c39acaf
fokrul-islam <fxylxy@googlemail.com>
2020-02-08 10:32:25 +0000

    na

--- testscript.sh
+++ testscript.sh
@@ -1,6 +1,5 @@
 echo "Hello from test script from github"
 NUMBER=$((NUMBERONE+NUMBERTWO))
 echo "total is: $NUMBER ------:-)"
-echo "competed :-)"
 echo "a nbew line was added"
 echo $DATE"   ---->"
