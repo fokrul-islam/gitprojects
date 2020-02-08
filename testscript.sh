7832b2f06af0dc0b3c60dde7b51e8343d39056e4
fokrul-islam <fxylxy@googlemail.com>
2020-02-08 10:15:12 +0000

    added smiley

--- testscript.sh
+++ testscript.sh
@@ -1,5 +1,5 @@
 echo "Hello from test script from github"
 NUMBER=$((NUMBERONE+NUMBERTWO))
-echo "total is: $NUMBER"
+echo "total is: $NUMBER ------:-)"
 echo "competed :-)"
 echo "a nbew line was added"
